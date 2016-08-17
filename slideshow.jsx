97641a3997f1f0f453c836e2f765154387365814
Nik Nyby <nnyby@columbia.edu>
2016-08-17 14:14:27 -0400

    first commit

---
+++ slideshow.jsx
@@ -0,0 +1,108 @@
+var slides = [
+    {
+        'title': '',
+        'img': 'img/1.png'
+
+    },
+    {
+        'title': '',
+        'img': 'img/image.jpg'
+    },
+    {
+        'title': '',
+        'img': null
+    }
+];
+
+
+var PrevButton = React.createClass({
+    render: function() {
+        return (
+            <button className="prev"
+                    onClick={this.handleClick}>
+                previous
+            </button>
+        );
+    },
+    handleClick: function(e) {
+        this.props.slideshow.prevSlide();
+    }
+});
+
+var NextButton = React.createClass({
+    render: function() {
+        return (
+            <button className="next"
+                    onClick={this.handleClick}>
+                next
+            </button>
+        );
+    },
+    handleClick: function(e) {
+        this.props.slideshow.nextSlide();
+    }
+});
+
+var SlideDisplay = React.createClass({
+    render: function() {
+        var slide = this.props.slides[this.props.currentSlide];
+        console.log(window.innerHeight);
+        return (
+            <div className="slide">
+                <div className="img"
+                     style={{
+                         backgroundImage: 'url(' + slide.img + ')',
+                         height: window.innerHeight
+                     }} />
+            </div>
+        );
+    }
+});
+
+var Slideshow = React.createClass({
+    getInitialState: function() {
+        return {
+            'currentSlide': 0
+        };
+    },
+    prevSlide: function() {
+        var newSlide = this.state.currentSlide;
+        if (this.state['currentSlide'] > 0) {
+            newSlide = newSlide - 1;
+        }
+        this.setState({'currentSlide': newSlide});
+    },
+    nextSlide: function() {
+        var newSlide = this.state.currentSlide;
+        if (this.state.currentSlide < slides.length - 1) {
+            newSlide = newSlide + 1;
+        }
+        this.setState({'currentSlide': newSlide});
+    },
+    componentWillMount: function() {
+        document.addEventListener('keydown', this.handleKeyDown, false);
+    },
+    handleKeyDown: function(e) {
+        if (e.keyCode === 37) {
+            this.prevSlide();
+        } else if (e.keyCode === 39) {
+            this.nextSlide();
+        }
+    },
+    render: function() {
+        return (
+            <div className="slideshow" onKeyPress={this.handleKeyDown}>
+                <SlideDisplay
+                    slides={slides}
+                    currentSlide={this.state.currentSlide} />
+                <PrevButton slideshow={this} />
+                <NextButton slideshow={this}  />
+            </div>
+        );
+    }
+});
+
+ReactDOM.render(
+    <Slideshow />,
+    document.getElementById('slideshow')
+);
